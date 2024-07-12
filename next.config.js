53d6b89d98103e2282f760b7d591d69f5dc47799
Emran-Y <emranyonas602@gmail.com>
2024-07-12 20:32:02 +0300

    done

--- next.config.js
+++ next.config.js
@@ -1,35 +1,35 @@
 const withPWA = require("next-pwa")({
-    dest: 'public',
+  dest: "public",
 });
 
 module.exports = withPWA({
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.mp3$/,
-            use: {
-                loader: "url-loader",
-            },
-        });
-        return config;
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: "url-loader",
+      },
+    });
+    return config;
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+    unoptimized: true,
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+    future: {
+      exclude: [
+        /\.DS_Store/,
+        "Thumbs.db",
+        ".git",
+        ".github",
+        ".vscode",
+        "node_modules",
+      ],
     },
-    images: {
-        domains: ["res.cloudinary.com"],
-        unoptimized: true, 
-    },
-    pwa: {
-        dest: "public",
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === "development",
-        future: {
-            exclude: [
-                /\.DS_Store/,
-                "Thumbs.db",
-                ".git",
-                ".github",
-                ".vscode",
-                "node_modules",
-            ],
-        },
-    },
+  },
 });
